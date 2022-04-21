cfad7836178d668caddccfe46b536589a54e70df
RRamses <97097453+RRamses@users.noreply.github.com>
2022-04-21 17:40:11 +0000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,51 @@
+
+
+INSERT INTO Produt
+(
+    product_id 
+    name
+    Category
+    price
+)
+VALUES
+(
+PD1 , PD2 ,
+Samsung Galaxy S20 ,ASUS Notebook
+Smartphone, PC  ,
+32922 , 4599
+
+);
+
+
+INSERT INTO Customer
+(
+    customer_id 
+    name
+    tel 
+)
+VALUES
+(
+    CO1 , CO2 ,
+ALI ,ASMA ,
+71321009 , 77345823 ,
+
+);
+
+
+INSERT INTO Orders
+(
+    customer_id 
+    product_id
+    OrderDate
+    quantity
+    total_ammount
+)
+VALUES
+(
+ CO1 , CO2 .
+ PO2 , PO2
+NULL , 28/05/2020 ,
+2 , 1 ,
+9198 ? 3299?
+
+);
